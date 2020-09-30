0def1ef24467b18ae75054c89fddd0a125603a83
Analee Guerra <aguerra1508@gmail.com>
2020-09-29 20:33:37 -0500

    Employee constructor class with methods to pass employee test files

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,30 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    // getName Method
+    getName() {
+        return this.name;
+    }
+
+    // getID Method
+    getId() {
+        return this.id;
+    }
+
+    //getEmail Method 
+    getEmail() {
+        return this.email;
+    }
+
+    //getRole Method
+    getRole() {
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
